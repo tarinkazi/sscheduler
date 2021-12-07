62962906d8cae116c7586984c5305b5bf7dfc28d
Tarin Kazi <tarin.kazi.afrin@gmail.com>
2021-12-07 14:26:42 -0500

    test added for listitem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import classNames from "classnames"
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  // let buttonClass = "button";
+  const buttonClass = classNames("button", {
+   "button--confirm": props.confirm,
+   "button--danger": props.danger
+ });
+  
+
+  return (<button 
+  disabled={props.disabled} 
+  className={buttonClass} 
+  onClick = {props.onClick} 
+  >
+     {props.children}
+</button>
+  );
+
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "components/DayListItem.scss"
+import classNames from "classnames";
+
+export default function DayListItem(props){
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots ===0 
+  }); 
+
+  const formatSpots=function(spots){
+      if (spots === 0) {
+        return 'no spots remaining';
+      }
+      if (spots === 1) {
+        return `${spots} spot remaining`;
+        
+      }
+      if (spots > 1) {
+        return `${spots} spots remaining`;
+        
+      }
+  }
+  return (
+    <li className ={dayClass} 
+    onClick={() => props.setDay(props.name)}
+    >
+      <h2 className="text--regular" >{props.name}</h2>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
+    </li>
+  ); 
+}
